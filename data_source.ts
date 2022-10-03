1645f987363fa1605b3ec5b761ac63367eea3d09
GabrielSilva117 <gabriel.f.silva117@gmail.com>
2022-10-03 19:20:38 -0300

    build: changed datasource options

--- data_source.ts
+++ data_source.ts
@@ -1,4 +1,15 @@
-import { DataSource } from "typeorm";
+import { Users } from './src/entities/user'
+import { DataSource } from 'typeorm'
+import { User_documents } from './src/entities/user_document'
+import { Document_types } from './src/entities/document_type'
+import { Tags } from './src/entities/tags'
+import { Entries_tags } from './src/entities/entries_tags'
+import { Entries } from './src/entities/entries'
+import { Entry_types } from './src/entities/entry_types'
+import { Entry_categories } from './src/entities/entry_categories'
+import { Entry_items } from './src/entities/entry_items'
+import { Entry_images } from './src/entities/entry_images'
+import { Entry_subcategories } from './src/entities/entry_subcategories'
 
 export const MainDs = new DataSource({
   type: 'postgres',
@@ -7,9 +18,22 @@ export const MainDs = new DataSource({
   username: 'postgres',
   password: undefined,
   database: 'money_saver',
-  synchronize: true,
+  synchronize: false,
   logging: true,
-  entities: [],
+  entities: [
+    Users,
+    User_documents,
+    Document_types,
+    Tags,
+    Entries_tags,
+    Entries,
+    Entry_types,
+    Entry_categories,
+    Entry_items,
+    Entry_images,
+    Entry_subcategories
+  ],
   subscribers: [],
-  migrations: []
+  migrations: [],
+  migrationsTableName: './migrations/create_entity.ts'
 })
