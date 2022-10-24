b3f5e248d0007cdc3d2dd29895b62f69eebe5b10
GabrielSilva117 <gabriel.f.silva117@gmail.com>
2022-10-24 11:10:42 -0300

    feat: added a env variable for the port number

--- index.ts
+++ index.ts
@@ -1,22 +1,19 @@
 import { MainDs } from './data_source'
 import express from 'express'
 import { Register_User } from './src/routes/register_user'
-import { User_Authenticate } from './src/routes/login'    
+import { User_Authenticate } from './src/routes/login'
 
 const app = express()
-const port = process.env.port || '3000'
+const port = process.env.PORT || '3000'
 
 const main = async () => {
   try {
     MainDs.initialize()
     console.log('Connected to the Database')
     app.use(express.json())
-    app.use([
-      Register_User,
-      User_Authenticate
-    ])
+    app.use([Register_User, User_Authenticate])
     app.listen(port, () => {
-      console.log(`Server running on ${port}`)
+      console.log(`Server running on port: ${port}`)
     })
   } catch (err) {
     console.error('An error occured! ' + err)
