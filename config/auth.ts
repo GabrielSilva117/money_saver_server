25410b6f9db6c9399dbfdc28f60009606cb1f40e
GabrielSilva117 <gabriel.f.silva117@gmail.com>
2022-11-28 11:31:01 -0300

    feat: Changed the access token expire time

--- config/auth.ts
+++ config/auth.ts
@@ -6,7 +6,7 @@ export const accessTkn:string = process.env.ACCESS_TOKEN_SECRET || ''
 export const refreshTkn:string = process.env.REFESH_TOKEN_SECRET || ''
 
 export const generateToken = async (data: any) => {
-  return jwt.sign(data, accessTkn, { expiresIn: '15m' })
+  return jwt.sign(data, accessTkn, { expiresIn: '10m' })
 }
 
 export const generateRefreshToken = async (data: any) => {
